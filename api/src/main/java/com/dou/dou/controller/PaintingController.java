e6ac117c728658895bf6140e169c1b7647e3f7a4
何 奕文 <even.he@bizreach.co.jp>
2019-09-01 15:53:46 +0900

    added some dummy properties (greeting message) a user can configure when use the app. still wondering if there's some better way not to write a docker-compose file in order to deploy the app using ecs-cli since it's really surplus when you already need to write a task definition file.

--- api/src/main/java/com/dou/dou/controller/PaintingController.java
+++ api/src/main/java/com/dou/dou/controller/PaintingController.java
@@ -1,6 +1,7 @@
 package com.dou.dou.controller;
 
 import com.dou.dou.appService.model.Painting;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,14 @@ import java.util.List;
 
 @RestController
 public class PaintingController implements PaintingsApi {
+    @Value("${dou.greeting-message}")
+    private String greetingMessage;
+
     @Override
     public ResponseEntity<List<Painting>> paintingsGet() {
         return ResponseEntity.ok(
                 List.of(new Painting()
-                        .name("Sunflowers +1"))
+                        .name("Sunflowers +1. " + greetingMessage))
         );
     }
 }
