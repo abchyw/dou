6459e6ac9740359c814a56bf29fd86b00a41462e
He Even <wqhyw@126.com>
2019-09-09 15:16:30 +0900

    added code build project for front end

---
+++ terraform/aws/services/front/dou/code_build/deploy/local.tf
@@ -0,0 +1,4 @@
+locals {
+  source_repository = "https://github.com/abchyw/dou"
+  assume_role_policy = file("./assume_role_policy.json")
+}
---
+++ terraform/aws/services/front/dou/code_build/deploy/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  version = "2.23.0"
+}
+
+terraform {
+  backend "s3" {
+  }
+}
+
+resource "aws_iam_role" "code-build-dou-front-deploy-role" {
+  name = "code-build-dou-front-deploy"
+  assume_role_policy = local.assume_role_policy
+
+  tags = {
+    Name = "code-build-dou-front-deploy"
+  }
+}
+
+resource "aws_codebuild_project" "dou-front-deploy" {
+  name = "dou-front"
+  description = "deploy front end."
+  service_role = "${aws_iam_role.code-build-dou-front-deploy-role.arn}"
+
+// what is this?
+  artifacts {
+    type = "NO_ARTIFACTS"
+  } 
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/docker:18.09.0"
+    type = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+      type = "GITHUB"
+      location = local.source_repository
+      git_clone_depth = 1
+      buildspec = "front/buildspec.yml"
+  }
+
+  tags = {
+      Name = "code-build-dou-front-deploy"
+  }
+}
